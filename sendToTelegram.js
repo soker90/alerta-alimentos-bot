df62ed34295022abadca17d8d9004187e64f0ef0
Eduardo Parra <edue17@gmail.com>
2022-10-14 18:57:57 +0200

    Update sendToTelegram.js

--- sendToTelegram.js
+++ sendToTelegram.js
@@ -5,6 +5,6 @@ const bot = new TelegramBot(TOKEN_TELEGRAM)
 const CHAT_ID = process.env.CHAT_ID
 
 export const sendToTelegram = async ({ title, url }) => {
-  const msg = `${title} https://www.aesan.gob.es${url}`
-  await bot.sendMessage(CHAT_ID, msg)
+  const msg = `${title} [https://www.aesan.gob.es${url}](https://www.aesan.gob.es${url})`
+  await bot.sendMessage(CHAT_ID, msg, { parse_mode: 'Markdown' })
 }
