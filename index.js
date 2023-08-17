149daae6a8bcafb4cf12696ea28cef942d869f69
Eduardo Parra <eduardo.parramazuecos@telefonica.com>
2023-08-17 18:16:02 +0200

    Fix bug web aesan

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { chromium } from 'playwright-chromium'
 import { readFile, writeFile } from './fileUtils.js'
 import { sendToTelegram } from './sendToTelegram.js'
 
+// Hay un bug que deja un enlace sin texto al principio de la página
+// que provoca que no detecte nuevas entradas
+const LINK_WITH_BUG = '/AECOSAN/web/seguridad_alimentaria/ampliacion/2023_16.htm'
+
 const checkLastNew = async ({ content, index, lastPostUrlSaved }) => {
   let lastPostUrl
   try {
@@ -20,6 +24,14 @@ const checkLastNew = async ({ content, index, lastPostUrlSaved }) => {
   return false
 }
 
+const purgeBugLink = async (content) => {
+  const bugLink = await content[0].getAttribute('href')
+
+  if (bugLink === LINK_WITH_BUG) {
+    content.shift()
+  }
+}
+
 const URL_ALERTS = 'https://www.aesan.gob.es/AECOSAN/web/seguridad_alimentaria/subseccion/otras_alertas_alimentarias.htm'
 const browser = await chromium.launch({ headless: true })
 
@@ -27,6 +39,7 @@ const page = await browser.newPage()
 await page.goto(URL_ALERTS)
 
 const content = await page.$$('.theContent > p > a')
+await purgeBugLink(content)
 const lastPostUrlSaved = await readFile()
 
 const lastNews = await checkLastNew({ content, index: 0, lastPostUrlSaved })
