5297c890c4241181e95d680f1cf18a89504c3c6e
Eduardo Parra <edue17@gmail.com>
2026-04-07 19:05:18 +0200

    fix bug duplicated messages with same link

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ import { parse } from 'node-html-parser'
 import { readFile, writeFile } from './fileUtils.js'
 import { sendToTelegram } from './sendToTelegram.js'
 
-// Hay un bug que deja un enlace sin texto al principio de la página
-// que provoca que no detecte nuevas entradas
-const LINK_WITH_BUG = '/AECOSAN/web/seguridad_alimentaria/ampliacion/2023_16.htm'
-
 const checkLastNew = async ({ content, index, lastPostUrlSaved }) => {
   if (!content[index]) return true
 
-  const lastPostUrl = content[index].getAttribute('href')
-  const text = content[index].innerText
+  const lastPostUrl = content[index].querySelector('a').getAttribute('href')
+  const text = content[index].text.trim()
 
   if (lastPostUrl !== lastPostUrlSaved.lastNews) {
     console.log(`Nueva noticia: ${text} - ${lastPostUrl}\n`)
@@ -20,22 +16,13 @@ const checkLastNew = async ({ content, index, lastPostUrlSaved }) => {
   return false
 }
 
-const purgeBugLink = (content) => {
-  const bugLink = content[0].getAttribute('href')
-
-  if (bugLink === LINK_WITH_BUG) {
-    content.shift()
-  }
-}
-
 const URL_ALERTS = 'https://www.aesan.gob.es/AECOSAN/web/seguridad_alimentaria/subseccion/otras_alertas_alimentarias.htm'
 
 const res = await fetch(URL_ALERTS)
 const html = await res.text()
 const root = parse(html)
 
-const content = root.querySelectorAll('.theContent > p > a')
-purgeBugLink(content)
+const content = root.querySelectorAll('.theContent > p').filter(el => el.querySelector('a'))
 const lastPostUrlSaved = await readFile()
 
 const lastNews = await checkLastNew({ content, index: 0, lastPostUrlSaved })
--- sendToTelegram.js
+++ sendToTelegram.js
@@ -4,12 +4,15 @@ const TOKEN_TELEGRAM = process.env.TOKEN_TELEGRAM
 const bot = new TelegramBot(TOKEN_TELEGRAM)
 const CHAT_ID = process.env.CHAT_ID
 
+const escapeMarkdownV2 = (text) => text.replace(/[_*[\]()~`>#+=|{}.!\-\\]/g, '\\$&')
+
 export const sendToTelegram = async ({ title, url }) => {
   if (!TOKEN_TELEGRAM || !CHAT_ID) {
     console.error('TOKEN_TELEGRAM o CHAT_ID no están definidos en las variables de entorno.')
     return
   }
-  
-  const msg = `${title} [https://www.aesan.gob.es${url}](https://www.aesan.gob.es${url})`
-  await bot.sendMessage(CHAT_ID, msg, { parse_mode: 'Markdown' })
+
+  const fullUrl = `https://www.aesan.gob.es${url}`
+  const msg = `[${escapeMarkdownV2(title)}](${fullUrl})`
+  await bot.sendMessage(CHAT_ID, msg, { parse_mode: 'MarkdownV2' })
 }
