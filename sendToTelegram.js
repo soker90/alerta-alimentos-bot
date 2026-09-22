78ded7de8ea966000674bd10a256de3478f9f9cf
Eduardo Parra <edue17@gmail.com>
2026-09-23 00:14:16 +0200

    Handle new AESAN alert URLs

--- sendToTelegram.js
+++ sendToTelegram.js
@@ -4,7 +4,7 @@ const TOKEN_TELEGRAM = process.env.TOKEN_TELEGRAM
 const bot = new TelegramBot(TOKEN_TELEGRAM)
 const CHAT_ID = process.env.CHAT_ID
 
-const escapeMarkdownV2 = (text) => text.replace(/[_*[\]()~`>#+=|{}.!\-\\]/g, '\\$&')
+const escapeMarkdownV2 = (text) => text.replace(/[_*[\\]()~`>#+=|{}.!\-\\]/g, '\\$&')
 
 export const sendToTelegram = async ({ title, url }) => {
   if (!TOKEN_TELEGRAM || !CHAT_ID) {
@@ -12,7 +12,7 @@ export const sendToTelegram = async ({ title, url }) => {
     return
   }
 
-  const fullUrl = `https://www.aesan.gob.es${url}`
+  const fullUrl = new URL(url, 'https://www.aesan.gob.es').href
   const msg = `[${escapeMarkdownV2(title)}](${fullUrl})`
   await bot.sendMessage(CHAT_ID, msg, { parse_mode: 'MarkdownV2' })
 }
